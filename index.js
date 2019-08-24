7ec10788ef8e116fa4ad20ea7207bc48ffb8cfb2
yash desai <yhdesai99@ymail.com>
2019-08-24 15:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function setCart(c) {
 function addToCart(item) {
   var price = Math.floor(Math.random() * (100 - 1)) + 1
   var cartItem = {itemName: item, itemPrice: price}
-  
+
   cart.push(cartItem)
-  
+
   return `${item} has been added to your cart.`
-  
+
 }
 
 function viewCart() {
